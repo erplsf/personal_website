189826d53c196bc122a58ad6d5c854d234861ba9
Andriy Mykhaylyk <erp.lsf@gmail.com>
2020-06-02 07:43:28 +0200

    stash ideas

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,14 +2,21 @@ import React from "react"
 import { Helmet } from "react-helmet"
 import "../assets/style.css";
 
+//     <div> Solve. Improve. Repeat. </div>
+
 export default () => (
   <>
     <Helmet>
       <meta charSet="utf-8" />
         <title>Andriy Mykhaylyk</title>
     </Helmet>
-    <div className="container mx-auto">
-      <div className="font-mono text-lg text-center">Solve. Improve. Repeat</div>
+    <div className="bg-gray-300">
+    <div className="font-serif text-lg"> Solve. Improve. Repeat. </div>
+    <div className="flex flex-row">
+    <div className="w-2/5 font-sans text-lg text-center bg-green-300">
+    <div> Hello, My name is _Andriy Mykhaylyk_. I help to *solve your problems*. </div>
+    </div>
+    </div>
     </div>
   </>
 )
